f5eb6d609247808790cf1562d5b56704534f0e72
MarcusOkodugha <100699584+MarcusOkodugha@users.noreply.github.com>
2024-04-30 18:01:52 +0200

    type writer utan --host

--- src/components/Typewriter.tsx
+++ src/components/Typewriter.tsx
@@ -1,5 +1,5 @@
 import '../styles/Title.css';
-import React, {useState, useEffect} from 'react';
+import {useState, useEffect} from 'react';
 import { TypeAnimation } from 'react-type-animation';
 
 function Typewriter() {
